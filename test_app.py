30b01766b48fdf074e63263e9cfbfb9e05a13cff
SchonsG <gschonswow@gmail.com>
2022-02-19 19:19:42 -0300

    Improve code with dependency injection

--- test_app.py
+++ test_app.py
@@ -11,11 +11,17 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent
 
 
 def test_read():
-    app = App()
+    hour = datetime.datetime.now().isoformat()
+    temperature = 14.52
+    temperature_by_hour = {hour: temperature}
 
-    for key, value in app.read(file_name=Path(BASE_DIR).joinpath('london.csv')).items():
-        assert datetime.datetime.fromisoformat(key)
-        assert value - 0 == value
+    data_source = MagicMock()
+    data_source.read.return_value = temperature_by_hour
+
+    app = App(data_source=data_source)
+
+    assert app.read(file_name='something.csv') == temperature_by_hour
+
 
 def test_draw(monkeypatch):
     plot_date_mock = MagicMock()
@@ -24,11 +30,10 @@ def test_draw(monkeypatch):
     monkeypatch.setattr(matplotlib.pyplot, 'plot_date', plot_date_mock)
     monkeypatch.setattr(matplotlib.pyplot, 'show', show_mock)
 
-    app = App()
+    app = App(MagicMock())
 
     hour = datetime.datetime.now().isoformat()
     temp = 14.52
-
     app.draw({hour: temp})
 
     _, called_temperatures = plot_date_mock.call_args[0]
