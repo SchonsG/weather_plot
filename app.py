4b1872d5b0f8b3c3360b4f9027be4d7e1badd57f
SchonsG <gschonswow@gmail.com>
2022-02-20 11:07:10 -0300

    Add plotly plot

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ class App:
 if __name__ == '__main__':
     import sys
     from open_weather_json import DataSource
-    from matplotlib_plot import Plot
+    from plotly_plot import Plot
 
     file_name = sys.argv[1]
     
